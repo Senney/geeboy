43f6cd9913440d5098f9565a7800ed73077ee207
Sean Heintz <sean.heintz@gmail.com>
2015-02-26 01:02:33 -0700

    Added CPU to main gameboy definition

--- src/geeboy.py
+++ src/geeboy.py
@@ -1,5 +1,6 @@
 import logging
 from cartridge import Cartridge
+from cpu import CPU
 from disassembler import Disassembler
 from instruction import OpcodeParser
 from mem import mbc1
@@ -14,6 +15,8 @@ class GeeBoy(object):
         self._codes.load_instructions("./dat/opcodes.json")
 
         self._mem = mbc1.MBC1(self._cartridge)
+        self._cpu = CPU(self._cartridge, self._mem, self._codes)
+        self._cpu.run()
 
 
 if __name__ == "__main__":
